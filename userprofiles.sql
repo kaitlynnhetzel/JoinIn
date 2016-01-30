f1c7e1ff03011beb0ab1165b02cf83b634c356d6
kaitlynnhetzel <knh33@pitt.edu>
2016-01-29 21:52:29 -0500

    addition of rest

--- userprofiles.sql
+++ userprofiles.sql
@@ -1,14 +1,14 @@
 --
 -- User table structure for Pitt JumpIn members
 --
-
 CREATE TABLE 'JumpIn_members' (
 	'id' int(11) NOT NULL auto_increment,
 	'user' varchar(32) collate utf8_unicode_ci NOT NULL default '',
-	'password'
-
-
-
-
-
+	'password' varchar(32) collate utf8_unicode_ci NOT NULL default '',
+	'email' varchar(255) collate utf8_unicode_ci NOT NULL default '',
+	'regIP' varchar(15) collate utf8_unicode_ci NOT NULL default '',
+	'dt' datetime NOT NULL default '0000-00-00 00:00:00';
+	PRIMARY KEY ('id'),
+	UNIQUE KEY 'usr' ('usr')
+	
 ) ENGINE=myISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
